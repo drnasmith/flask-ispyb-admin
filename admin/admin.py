fb5cfcc119f0daf1110639857e842a37ee24aca2
Neil A Smith <neil.smith@diamond.ac.uk>
2018-09-04 17:01:37 +0100

    added custom model view to avoid rendering binary externalId

--- admin/admin.py
+++ admin/admin.py
@@ -4,9 +4,11 @@ from flask_admin.contrib.sqla import ModelView
 from ispyb.model_list import model_list
 from ispyb import db
 
+class ISPyBModelView(ModelView):
+    column_exclude_list = ('externalId')
 
 def init_app(app):
     admin = Admin(app, name='ISPyB Models', template_mode='bootstrap3')
 
     for model in model_list:
-        admin.add_view(ModelView(model, db.session))
+        admin.add_view(ISPyBModelView(model, db.session))
