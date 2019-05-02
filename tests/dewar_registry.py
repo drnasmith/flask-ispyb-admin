eebf7ee9489e22265aa7cd23263a3bb74efa9a86
Neil A Smith <neil.smith@diamond.ac.uk>
2019-05-02 17:28:25 +0100

    Add a number of registered dewars for testing

--- tests/dewar_registry.py
+++ tests/dewar_registry.py
@@ -7,8 +7,8 @@ from sqlalchemy.orm.exc import UnmappedInstanceError
 from ispyb import models
 
 from tests import setup
-from tests import test_labcontact
-from tests import test_proposal
+from tests import labcontact
+from tests import proposal
 
 def create(session, params):
     """
@@ -31,17 +31,22 @@ def create(session, params):
 
 if __name__ == "__main__":
 
+    facilityCodes = [ '{:0>4d}'.format(x) for x in range(30)]
+    print(facilityCodes)
+
     session = setup.create_db_session()
 
-    lc_params = {'card_name': 'Blah'}
-    labcontact_id = test_labcontact.get_id(session, lc_params)
-    proposal_id = test_proposal.get_id(session, "cm", "1001")
+    lc_params = {'card_name': 'LC1'}
+    labcontact_id = labcontact.get_id(session, lc_params)
+    proposal_id = proposal.get_id(session, "cm", "14451")
 
     if labcontact_id and proposal_id:
-        params = { "proposal_id": proposal_id,
-            "labcontact_id": labcontact_id,
-            "facility_code": "DLS-MX-1001"}
+        for fc in facilityCodes:
+            params = { "proposal_id": proposal_id,
+                "labcontact_id": labcontact_id,
+                "facility_code": "DLS-MX-{}".format(fc)}
+            print("Adding facility code DLS-MX-{} to prop {}".format(fc, proposal_id))
 
-        create(session, params)
+            create(session, params)
     else:
         print("Error could not get proposal or labcontact id")
